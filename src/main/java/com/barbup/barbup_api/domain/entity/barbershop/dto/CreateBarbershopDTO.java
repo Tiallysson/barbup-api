c829b16a61a4f585c99d479403d0dbc0bea5b369
Tiallysson Costa <tiallysson@proton.me>
2026-08-29 14:12:11 -0300

    fix: change validation for null entity

--- src/main/java/com/barbup/barbup_api/domain/entity/barbershop/dto/CreateBarbershopDTO.java
+++ src/main/java/com/barbup/barbup_api/domain/entity/barbershop/dto/CreateBarbershopDTO.java
@@ -3,7 +3,9 @@ package com.barbup.barbup_api.domain.entity.barbershop.dto;
 import com.barbup.barbup_api.domain.entity.address.dto.AddressDto;
 import com.barbup.barbup_api.domain.entity.barbershop.validation.PhoneNumber;
 import com.barbup.barbup_api.domain.entity.barbershop.validation.Zipcode;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import org.hibernate.validator.constraints.br.CPF;
 
@@ -20,6 +22,8 @@ public record CreateBarbershopDTO(
         String phone,
         String logoUrl,
         String userId,
+        @NotNull(message = "Address cannot possible null")
+        @Valid
         AddressDto address
 ) {
 }
