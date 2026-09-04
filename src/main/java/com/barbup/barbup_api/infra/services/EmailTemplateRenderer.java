0262e9d2e5868013e14e12588a60bfe517e39b13
Tiallysson Costa <tiallysson@proton.me>
2026-09-03 23:14:51 -0300

    feat: add mailjet service for tests

--- src/main/java/com/barbup/barbup_api/infra/services/EmailTemplateRenderer.java
+++ src/main/java/com/barbup/barbup_api/infra/services/EmailTemplateRenderer.java
@@ -10,7 +10,7 @@ import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
-public class EmailTemplareRenderer {
+public class EmailTemplateRenderer {
 
     private final TemplateEngine templateEngine;
 
