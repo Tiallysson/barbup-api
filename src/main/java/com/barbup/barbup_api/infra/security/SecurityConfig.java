00f4e173affa293c42f089a71bc0b03863c537a3
Tiallysson Santos da Costa <tiallysson@proton.me>
2026-09-05 22:42:22 -0300

    fix: modify the Spring Security configuration to permit unauthenticated access to password reset endpoints. (#23)

--- src/main/java/com/barbup/barbup_api/infra/security/SecurityConfig.java
+++ src/main/java/com/barbup/barbup_api/infra/security/SecurityConfig.java
@@ -32,6 +32,9 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/confirm-email").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/verify-code").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/forgot-password").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/reset-password").permitAll()
                         .requestMatchers(
                                 "/scalar.html",
                                 "/v3/api-docs/**"
